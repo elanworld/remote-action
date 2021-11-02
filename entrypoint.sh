7d0fe892aa81b4205d0066839845d319d11e653d
Alan <elanworld@163.com>
2021-11-02 17:31:32 +0800

    添加http服务方式

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
-if [[ -z "$NGROK_TOKEN" ]]; then
+if [[ -z "$INPUT_NGROK_TOKEN" ]]; then
   echo "Please set 'NGROK_TOKEN'"
   exit 2
 fi
 
-if [[ -z "$USER_PASS" ]]; then
+if [[ -z "$INPUT_USER_PASSWD" ]]; then
   echo "Please set 'USER_PASS' for user: $USER"
   exit 3
 fi
 
 echo "### Install ngrok ###"
-
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
 
 echo "### Update user: $USER password ###"
-echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
-
-echo "### Start ngrok proxy for 22 port ###"
+echo -e "$INPUT_USER_PASSWD\n$INPUT_USER_PASSWD\n" | sudo passwd "$USER"
 
 
-rm -f .ngrok.log
-./ngrok authtoken "$NGROK_TOKEN"
-./ngrok tcp 22 --log ".ngrok.log" &
+if [[ "http" == "$INPUT_NGROK_TYPE" ]]; then
+  echo "### Start ngrok proxy for 80 port ###"
+  rm -f .ngrok.log
+  ./ngrok authtoken "$INPUT_NGROK_TOKEN"
+  ./ngrok http 80 --log ".ngrok.log" &
+else
+  echo "### Start ngrok proxy for 22 port ###"
+  rm -f .ngrok.log
+  ./ngrok authtoken "$INPUT_NGROK_TOKEN"
+  ./ngrok tcp 22 --log ".ngrok.log" &
+fi
+
 
 sleep 10
-HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+HAS_ERRORS=$(grep "command failed" <.ngrok.log)
 
 if [[ -z "$HAS_ERRORS" ]]; then
-  echo ""
   echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "connect: $(grep -o -E "(tcp|http|https)://(.+)" < .ngrok.log)"
   echo "=========================================="
   keepAliveFile=~/keepAlive
-  echo run touch $keepAliveFile
+  echo 'run touch ~/keepAlive to keep network alive'
   sleep 600
-  while [ -s $keepAliveFile ];
-  do
-      sleep 10
+  while [ -f $keepAliveFile ]; do
+    sleep 10
   done
   exit
 else
