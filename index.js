09234887acdeaa6b815e59f549526d2920d97def
Alan <elanworld@163.com>
2021-11-02 14:30:17 +0800

    添加端口暴露服务

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('child_process').exec;
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+
+// `who-to-greet` input defined in action metadata file
+const nameToGreet = core.getInput('who-to-greet');
+console.log(`Hello ${nameToGreet}!`);
+const time = (new Date()).toTimeString();
+core.setOutput("time", time);
+// Get the JSON webhook payload for the event that triggered the workflow
+const payload = JSON.stringify(github.context.payload, undefined, 2)
+console.log(`The event payload: ${payload}`);
+exec('bash entrypoint.sh',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+            throw error
+        }
+    });
